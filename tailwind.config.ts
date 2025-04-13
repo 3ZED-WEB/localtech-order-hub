ca01fcb96bd43875775405ac2974d99dd6f332f6
3ZED-WEB <3zed.group@gmail.com>
2025-04-13 16:07:19 +0000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,8 +54,8 @@ export default {
 				},
 				
 				// Custom colors for 3zedshop
-				dark: "#1A1F2C",
-				darker: "#12151e",
+				dark: "#090705",
+				darker: "#040303",
 				red: "#c4071c",
 				"red-light": "#d65667",
 				"red-dark": "#a01c2d",
